9ec3b859280bef02c778e1e7e3600e597e1ff07d
Osama Ghazal <osama.gazal.1996@gmail.com>
2021-05-06 15:29:24 +0300

    init state

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+
 # ecommerce-flutter-arcore
 A e-commerce application using flutter and support arcore for viewing goods in the user environment 
+
---
+++ android/app/src/main/kotlin/com/og/ecommerce_arcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.og.ecommerce_arcore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
