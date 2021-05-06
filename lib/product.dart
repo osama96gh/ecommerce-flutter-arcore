9805b076ad5ad21cf23b024b23c07ef6a36272f2
Osama Ghazal <osama.gazal.1996@gmail.com>
2021-05-06 18:21:46 +0300

    add product class to hold the info of the products

---
+++ lib/product.dart
@@ -0,0 +1,10 @@
+class Product {
+  String title;
+  String description;
+  String imageUrl;
+  String modelUrl;
+  double price;
+
+  Product(
+      this.title, this.description, this.imageUrl, this.modelUrl, this.price);
+}
---
+++ lib/utility.dart
@@ -0,0 +1,31 @@
+import 'package:ecommerce_arcore/product.dart';
+
+List<Product> getTempProduct() {
+  List<Product> temp = [];
+  temp.add(Product(
+      'Centiar Dining Chair',
+      "Mid-century design, updated for today's lifestyle. Centiar upholstered dining chairs come to the table with more than just good looks. They're covered with high performance Nuvella® fabric, which offers unbeatable stain resistance.",
+      "https://drive.google.com/uc?export=download&id=12wj-N2Q5relKn7VDYgUOUGXit4NVJvtf",
+      "modelUrl",
+      99.9));
+
+  Product(
+      'Berringer Dining Bench',
+      "Berringer serves up easy elegance, family style. The rich, rustic finish and simple profile are always in vogue, whether your aesthetic is vintage, country or traditional. Dining room bench comfortably seats two.",
+      "https://drive.google.com/uc?export=download&id=1nzRMCgF-0yGPQkxDeOs23_NpLQDYdSnS",
+      "modelUrl",
+      124);
+
+  Product(
+      'Berringer Dining Drop Leaf Extendable Table',
+      "The Berringer dining room table incorporates decidedly rustic flair. Two drop leaves provide just enough table space to accommodate the drop-in guest. Simple and clean lined, with stylish stilted legs, it’s a look that easily fits, whether your aesthetic is vintage, country or traditional.",
+      "https://drive.google.com/uc?export=download&id=1EFXrGXdpU2cV83dusYzphnp9LA0F1Qgm",
+      "modelUrl",
+      200);
+  Product(
+      'Hollyann Sofa',
+      "Cool takes a new shape in your living room with the Hollyann sofa. Rounding out your stylish, urban space, this piece sports a velvety soft upholstery and sleek, tapered legs. Curl up with comfort, and really turn around your home decor.",
+      "https://drive.google.com/uc?export=download&id=1Gb2RgeThSt6eXyJqWYzmr7wP75NKbad0",
+      "modelUrl",
+      177.5);
+}
